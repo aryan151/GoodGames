3f227499a41afa2c35f269f9d4331455ff9ef5c4
verykenny <16845292+verykenny@users.noreply.github.com>
2021-07-26 13:18:31 -0600

    normalized spacing in all files

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const {
-  db: { username, password, database, host },
+    db: { username, password, database, host },
 } = require('./index');
 
 module.exports = {
-  development: {
-    username,
-    password,
-    database,
-    host,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-  },
+    development: {
+        username,
+        password,
+        database,
+        host,
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+    },
 };
