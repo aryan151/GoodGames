44a4f25970f7bcf49e7ad7febdfb4dba335f435c
verykenny <16845292+verykenny@users.noreply.github.com>
2021-07-26 16:22:29 -0600

    created game model, migration, seeds

---
+++ db/migrations/20210726220556-create-game.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Games', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            title: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            description: {
+                allowNull: false,
+                type: Sequelize.TEXT
+            },
+            releaseDate: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Games');
+    }
+};
