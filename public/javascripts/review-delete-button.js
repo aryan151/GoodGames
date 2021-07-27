80243b9d5348f208c542cc6b51603c29207c4b49
Cam Chandler <camchandler15@gmail.com>
2021-07-27 18:58:29 -0400

    added event isteners

---
+++ public/javascripts/review-delete-button.js
@@ -0,0 +1,7 @@
+let deleteButtons = document.querySelectorAll('.delete-button')
+
+deleteButtons.forEach(button =>{
+    button.addEventListener('click',event =>{
+        console.log('for each worked')
+    })
+})
