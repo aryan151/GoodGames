7e3f114a001b18d9428e9179d121a74f0c682857
Cam Chandler <camchandler15@gmail.com>
2021-07-27 12:34:33 -0400

    added button respose

--- public/javascripts/demo-button.js
+++ public/javascripts/demo-button.js
@@ -1,14 +1,12 @@
 let demoButton = document.querySelector('#demo-container > button')
 
 demoButton.addEventListener('click', async event =>{
-    console.log('clicked demo')
 
    let res = await fetch('/demo',{
         method: 'post',
     })
 
 
-    if (res.redirected){
-        window.location.href = res.url
-    }
+    return window.location.href = res.url
+
 })
