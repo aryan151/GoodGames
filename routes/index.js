924d3ac0f1c68e8ed75257080cafcc72247922a4
verykenny <16845292+verykenny@users.noreply.github.com>
2021-07-27 16:37:51 -0600

    added root template

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const { asyncHandler } = require('./utils');
+const { Game, Review } = require('../db/models');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+    const games = await Game.findAll({
+        order: [['createdAt', 'DESC']],
+        limit: 10,
+        include: Review
+    })
+
+    res.render('index', {title: 'Newest Games', games})
+}));
 
 router.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
