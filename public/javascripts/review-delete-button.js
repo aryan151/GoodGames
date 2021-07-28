53e5d553e76d1fb855dbe2c0511eff67c85ce95c
Cam Chandler <camchandler15@gmail.com>
2021-07-27 23:04:38 -0400

    added functionaly to dynamically display a message when you have no reviews

--- public/javascripts/review-delete-button.js
+++ public/javascripts/review-delete-button.js
@@ -1,15 +1,44 @@
-let deleteButtons = document.querySelectorAll('.delete-button')
+window.addEventListener('DOMContentLoaded', event => {
+    let deleteButtons = document.querySelectorAll('.delete-button')
 
-deleteButtons.forEach(button =>{
-    button.addEventListener('click',async event =>{
-        '/reviews/:id(\\d+)/edit'
-        let id = event.target.id
-        let review = document.querySelector(`#card${id}`)
+    let container = document.querySelector('#reviews-container')
+    let noReviewDispaly = () =>{
+    let cards = document.querySelectorAll('.card')
 
-        await fetch(`/reviews/${id}/delete`,{
-            method: 'post',
+    if(cards.length === 0){
+        let emptyCard = document.createElement('div')
+        emptyCard.classList.add('card')
+
+        let emptyText = document.createElement('h2')
+        emptyText.classList.add('card-title')
+        emptyText.innerText ='You dont have any reviews right now'
+
+        let gamesLink = document.createElement('a')
+        gamesLink.href = '/'
+
+        let gamesLinkText = document.createElement('p')
+        gamesLinkText.innerText = 'Find a Game?'
+        gamesLinkText.id = 'games-link'
+
+        gamesLink.appendChild(gamesLinkText)
+        emptyCard.appendChild(emptyText)
+        emptyCard.appendChild(gamesLink)
+        container.appendChild(emptyCard)
+
+    }
+}
+    noReviewDispaly()
+    deleteButtons.forEach(button =>{
+        button.addEventListener('click',async event =>{
+            '/reviews/:id(\\d+)/edit'
+            let id = event.target.id
+            let review = document.querySelector(`#card${id}`)
+            await fetch(`/reviews/${id}/delete`,{
+                method: 'post',
+            })
+
+            review.remove()
+            noReviewDispaly()
         })
-
-        review.remove()
-    })
+})
 })
