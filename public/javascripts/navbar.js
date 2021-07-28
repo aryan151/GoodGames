2ca0705dd9f9cfa10666d440d6212647f6697254
Aryan Attul <aryanattula@gmail.com>
2021-07-28 18:07:47 -0400

    navbar demo fix

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', event => {
     const logoutLink = document.querySelector('#logout')
+    const demolink = document.querySelector('#demo')
 
     logoutLink.addEventListener('click', async event => {
         event.preventDefault();
@@ -9,5 +10,15 @@ window.addEventListener('DOMContentLoaded', event => {
         })
 
         return window.location.href = '/'
+    })
+
+    demolink.addEventListener('click', async event => {
+        event.preventDefault();
+
+        await fetch('/demo', {
+            method: 'post',
+        })
+
+        return window.location.href = res.url
     })
 })
