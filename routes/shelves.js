637a4cbe8821bee8ae216f7e9ec5891e439a572d
verykenny <16845292+verykenny@users.noreply.github.com>
2021-07-28 17:07:21 -0600

    added csrf to the GET /shelves router

--- routes/shelves.js
+++ routes/shelves.js
@@ -12,7 +12,7 @@ router.use(requireAuth)
 
 
 // GET ALL SHELVES
-router.get('/shelves', asyncHandler(async (req, res, next) => {
+router.get('/shelves', csrfProtection, asyncHandler(async (req, res, next) => {
     const { userId } = req.session.auth;
 
     const shelves = await UserShelf.findAll({
@@ -24,7 +24,7 @@ router.get('/shelves', asyncHandler(async (req, res, next) => {
             [{model: Game}, 'title']
         ]
     })
-    res.render('shelves', { title: 'Your Shelves', shelves });
+    res.render('shelves', { title: 'Your Shelves', shelves, csrfToken: req.csrfToken() });
 }));
 
 // GET LIST OF USER SHELVES
