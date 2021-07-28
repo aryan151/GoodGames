e84e0e65dfc6dfe7b4a0563c7f761e61c9266caa
Cam Chandler <camchandler15@gmail.com>
2021-07-27 20:45:02 -0400

    added review delete functionality

--- public/javascripts/review-delete-button.js
+++ public/javascripts/review-delete-button.js
@@ -1,7 +1,15 @@
 let deleteButtons = document.querySelectorAll('.delete-button')
 
 deleteButtons.forEach(button =>{
-    button.addEventListener('click',event =>{
-        console.log('for each worked')
+    button.addEventListener('click',async event =>{
+        '/reviews/:id(\\d+)/edit'
+        let id = event.target.id
+        let review = document.querySelector(`#card${id}`)
+
+        await fetch(`/reviews/${id}/delete`,{
+            method: 'post',
+        })
+
+        review.remove()
     })
 })
