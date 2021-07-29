02c832c56a64bb277b31dc89ea00460c1baa7c37
Cam Chandler <camchandler15@gmail.com>
2021-07-29 15:55:07 -0400

    shelves page now displays a message when a shelf doesnt have any games

--- public/javascripts/review-delete-button.js
+++ public/javascripts/review-delete-button.js
@@ -14,11 +14,11 @@ window.addEventListener('DOMContentLoaded', event => {
         emptyText.innerText ='You dont have any reviews right now'
 
         let gamesLink = document.createElement('a')
-        gamesLink.href = '/'
+        gamesLink.href = ''
 
         let gamesLinkText = document.createElement('p')
         gamesLinkText.innerText = 'Find a Game?'
-        gamesLinkText.id = 'games-link'
+        gamesLinkText.className = 'games-link'
 
         gamesLink.appendChild(gamesLinkText)
         emptyCard.appendChild(emptyText)
