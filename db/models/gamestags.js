80081f737477f0c9a3a21dc246e21be686d6c023
Cam Chandler <camchandler15@gmail.com>
2021-08-02 16:25:22 -0400

    changed a tag name

--- db/models/gamestags.js
+++ db/models/gamestags.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const GamesGenres = sequelize.define('GamesGenres', {
     gameId: DataTypes.INTEGER,
-    tagId: DataTypes.INTEGER
+    genreId: DataTypes.INTEGER
   }, {});
   GamesGenres.associate = function(models) {
     // associations can be defined here
